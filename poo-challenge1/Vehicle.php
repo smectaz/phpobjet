f509a63923f04ae016d61e07813a03bb0e26a186
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-01 10:41:41 +0100

    changement

---
+++ poo-challenge1/Vehicle.php
@@ -0,0 +1,87 @@
+<?php
+
+// Vehicle.php
+
+class Vehicle
+{
+ 
+    public string $color;
+
+    public int $currentSpeed;
+
+    public int $nbSeats;
+
+    public int $nbWheels;
+
+    public function __construct(string $color, int $nbSeats)
+    {
+        $this->color = $color;
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function start(): string
+{
+    $resultat = "C'est l'hiver ! Recommence ! Sinon Tu marche <br>";
+    return $resultat;
+}
+
+    public function forward(): string
+    {
+        $this->currentSpeed = 15;
+        return "Go ! <br>";
+    }
+
+    public function brake(): string
+    {
+        $sentence = "";
+        while ($this->currentSpeed > 0) {
+            $this->currentSpeed--;
+            $sentence .= "Brake !!!";
+        }
+
+        $sentence .= "<br> non BOUM!!!! dans le mur <br>";
+        return $sentence;
+    }
+
+    public function getCurrentSpeed(): int
+    {
+        return $this->currentSpeed;
+    }
+
+    public function setCurrentSpeed(int $currentSpeed): void
+    {
+        if($currentSpeed >= 0){
+            $this->currentSpeed = $currentSpeed;
+        }
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getNbSeats(): int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): void
+    {
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function getNbWheels(): int
+    {
+        return $this->nbWheels;
+    }
+
+    public function setNbWheels(int $nbWheels): void
+    {
+        $this->nbWheels = $nbWheels;
+    }
+}
---
+++ poo-challenge3/higway.php
@@ -0,0 +1,11 @@
+<?php
+
+abstract class Higway
+{
+    protected array $currentVehicles;
+    protected int $nbLane;
+    protected int $maxSpeed;
+}
+
+
+?>
