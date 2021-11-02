711c36258c4372ff86f4d61e251f0af65d081fa9
smectaz <90684643+smectaz@users.noreply.github.com>
2021-11-02 10:40:57 +0100

    challenge5

--- poo-challenge4/objet.php
+++ poo-challenge4/objet.php
@@ -3,7 +3,7 @@
 require_once 'Bicycle.php';
 require_once 'Car.php';
 require_once 'Truck.php';
-require_once 'Skateboard.php';
+require_once 'Skate.php';
 require_once 'HighWay.php';
 require_once 'MotorWay.php';
 require_once 'PedestrianWay.php';
